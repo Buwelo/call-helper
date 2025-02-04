8f17db529f2ca0af8e396ae407498b4300b5999e
calebba <bawacaleb14@gmail.com>
2025-02-04 15:40:22 -0500

    login update

--- controllers/authController.py
+++ controllers/authController.py
@@ -3,6 +3,8 @@ from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from config.extensions import db, bcrypt
 from models import User
+import random
+import string
 
 def register():
     if request.method == 'POST':
@@ -25,17 +27,34 @@ def register():
 
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+        first_name = request.form.get('first_name', '').lower()
+        last_name = request.form.get('last_name', '').lower()
+        email = request.form.get('email', '').lower()
+        # password = request.form.get('password', '')
+
+        if not first_name or not last_name or not email:
+            flash('First name, last name, and email are required.', 'danger')
+            return redirect(url_for('auth.login'))
         remember_me = 'remember' in request.form
         user = User.query.filter_by(email=email).first()
 
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user:
             login_user(user=user, remember=remember_me)
             flash('Logged in successfully!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Invalid email or password', 'danger')
+            random_digits = ''.join(random.choices(string.digits, k=2))
+            username = f"{first_name}_{last_name[:2]}{random_digits}"
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+            password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+
+            user = User(username=username, email=email, password=password_hash,
+                first_name=first_name, last_name=last_name)
+            print
+            db.session.add(user)
+            db.session.commit()
+            login_user(user=user, remember=remember_me)
+            return redirect(url_for('home'))
 
     return render_template('auth/login.html')
 
