3026252761bd7c13b8e6511cf7c8bc6068024f6e
Daniel Okyere <daniel.kwame.okyere101@gmail.com>
2025-02-10 21:33:46 +0000

    revisions from server

---
+++ migrations/versions/2220a2da4d16_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2220a2da4d16
+Revises: b7761be96279
+Create Date: 2025-02-10 16:34:58.365379
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2220a2da4d16'
+down_revision = 'b7761be96279'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/cd26fc68aefe_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: cd26fc68aefe
+Revises: 2220a2da4d16
+Create Date: 2025-02-10 16:37:01.138153
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd26fc68aefe'
+down_revision = '2220a2da4d16'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
