9d8854caed320689c2c5f464fcca0b36bea65f20
Sodja Nathan  O. <sodjanathan@gmail.com>
2025-02-21 15:45:50 +0000

    feat: modify model, migrate

---
+++ migrations/versions/cd26fc68aefe_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: cd26fc68aefe
+Revises: b08f1f8bfa5b
+Create Date: 2025-02-21 15:23:01.605205
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd26fc68aefe'
+down_revision = 'b08f1f8bfa5b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
