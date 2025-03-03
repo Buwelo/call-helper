e61450953278849ef315cd19039969a68e8a498a
Sodja Nathan  O <sodjanathan@gmail.com>
2025-03-03 18:59:44 +0000

    chore: migration- update transcript model

--- models/transcript.py
+++ models/transcript.py
@@ -4,13 +4,22 @@ from datetime import datetime, timezone
 
 class TranscriptTest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    good_transcript = db.Column(db.String(15000), nullable=False)  # Used to score test
-    bad_transcript = db.Column(db.String(15000), nullable=False)  # Used to generate test
-    audio_file_path = db.Column(db.String(200), nullable=True)  # Path to audio file for test
-    srt_file_path = db.Column(db.String(200), nullable=True)  # Path to srt file for test
-    name_of_test = db.Column(db.String(150), nullable=False)  # Name of the test
+    good_transcript = db.Column(
+        db.String(15000), nullable=False)  # Used to score test
+    bad_transcript = db.Column(
+        db.String(15000), nullable=False)  # Used to generate test
+    # Path to audio file for test
+    audio_file_path = db.Column(db.String(200), nullable=True)
+    # Path to srt file for test
+    srt_file_path = db.Column(db.String(200), nullable=True)
+    name_of_test = db.Column(
+        db.String(150), nullable=False)  # Name of the test
+    # score before any changes made to the transcript
+    benchmark_score = db.Column(db.Float, nullable=True)
+
     def __repr__(self):
         return f'<TranscriptTest {self.id}>'
+
     def serialize(self):
         return {
             'id': self.id,
@@ -18,20 +27,26 @@ class TranscriptTest(db.Model):
             'good_transcript': self.good_transcript,
             'bad_transcript': self.bad_transcript,
             'audio_file_path': self.audio_file_path,
+            'benchmark_score': self.benchmark_score,
             'srt_file_path': self.srt_file_path
         }
-    
-    
+
+
 class UserTranscript(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    score = db.Column(db.String(15000), nullable=False)  # Stores test scores in JSON format
-    test_taken = db.Column(db.Integer, db.ForeignKey('transcript_test.id'), nullable=False)
-    user_transcript = db.Column(db.String(15000), nullable=False)  # User's submitted transcript
+    # Stores test scores in JSON format
+    score = db.Column(db.String(15000), nullable=False)
+    test_taken = db.Column(db.Integer, db.ForeignKey(
+        'transcript_test.id'), nullable=False)
+    # User's submitted transcript
+    user_transcript = db.Column(db.String(15000), nullable=False)
     testing_id = db.Column(db.String(25), nullable=True)
-    benchmark_score= db.Column(db.Float, nullable=True)  # score before any changes made to the transcript
-    overall_score = db.Column(db.Float, nullable=True)  # Overall score of the transcript
-    summary = db.Column(db.String(15000), nullable=True)  # Summary of the transcript
-    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))  # Auto-set on creation
-    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=datetime.now())  # Auto-set on update
-
+    # Overall score of the transcript
+    overall_score = db.Column(db.Float, nullable=True)
+    # Summary of the transcript
+    summary = db.Column(db.String(15000), nullable=True)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(
+        timezone.utc))  # Auto-set on creation
+    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(
+        timezone.utc), onupdate=datetime.now())  # Auto-set on update
